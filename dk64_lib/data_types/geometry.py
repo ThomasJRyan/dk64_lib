682e7635e14c8122af34e3ffc5856bc10d81bd3c
Thomas Ryan <tomryan_8_1997@hotmail.com>
2023-01-19 00:07:09 -0500

    Cleaned code and fixed bug is triangle reading

--- dk64_lib/data_types/geometry.py
+++ dk64_lib/data_types/geometry.py
@@ -3,13 +3,11 @@
 import pathlib
 
 from io import FileIO
-from typing import Union
 from dataclasses import dataclass
 from tempfile import TemporaryFile
 
-from dk64_lib.components.vertex import Vertex
 from dk64_lib.data_types.base import BaseData
-from dk64_lib.f3dex2.commands import DL_COMMANDS, G_TRI1, G_VTX
+from dk64_lib.f3dex2.commands import DL_COMMANDS
 from dk64_lib.f3dex2.display_list import DisplayList
 from dk64_lib.file_io import get_bytes, get_char, get_long, get_short
 
@@ -28,17 +26,29 @@ class GeometryData(BaseData):
     @property
     def display_lists(self) -> list[DisplayList]:
         ret_list = list()
-        dl_start = self._dl_start
+        dl_start, vert_start = self._dl_start, self._vert_start
         with TemporaryFile() as data_file:
             data_file.write(self._raw_data)
             data_file.seek(dl_start)
-            raw_data = b""
+            raw_data, raw_vert_data = b"", b""
+            
             while data_file.tell() != self._vert_start:
-                command = get_bytes(data_file, 8)
-                raw_data += command
-                if command == b"\xDF\x00\x00\x00\x00\x00\x00\x00":
-                    ret_list.append(DisplayList(raw_data=raw_data, start=dl_start))
-                    raw_data = b""
+                command_bytes = get_bytes(data_file, 8)
+                
+                if (cls := DL_COMMANDS.get(command_bytes[:1])):
+                    cmd = cls(command_bytes)
+                else:
+                    continue
+                
+                raw_data += command_bytes
+                
+                if cmd.opcode == b"\x01":
+                    raw_vert_data += get_bytes(data_file, cmd.vertex_count * 16, vert_start, keep_last_pos=True)
+                    vert_start += cmd.vertex_count * 16
+                
+                if cmd.opcode == b"\xDF":
+                    ret_list.append(DisplayList(raw_data=raw_data, raw_vertex_data=raw_vert_data, offset=dl_start))
+                    raw_data, raw_vert_data = b"", b""
                     dl_start = data_file.tell()
         return ret_list
 
@@ -49,40 +59,32 @@ class GeometryData(BaseData):
             ret_list.extend(dl.triangles)
         return ret_list
 
-    def _parse_verticies(self, vertex_data: bytes, vertex_buffer: G_VTX):
-        ret_list = list()
-        vert_start = 0
-        vert_end = vert_start + 16
-        for _ in range(vertex_buffer.vertex_count):
-            vertex_bytes = vertex_data[vert_start:vert_end]
-            vertex = Vertex(vertex_bytes)
-            ret_list.append(vertex)
-            vert_start = vert_end
-            vert_end = vert_start + 16
-        return ret_list
-
     def _parse_data(self, fh: FileIO):
         self._dl_start = get_long(fh, 0x34)
         self._vert_start = get_long(fh, 0x38)
         self._dl_size = self._vert_start - self._dl_start
 
     def save_to_obj(self, filename: str, folderpath: str = "."):
+        """Save geometry data to obj format
+
+        Args:
+            filename (str): Name of obj file
+            folderpath (str, optional): Folder path to save obj to. Defaults to ".".
+        """
+        tri_offset = 1
         filepath = pathlib.Path(folderpath, filename)
-        face_offset = 1
-        vert_start = self._vert_start
+        
         with open(filepath, "w") as obj_file:
-            for display_list in self.display_lists:
-                for v_buff_num, vertex_buffer in enumerate(display_list.vertex_buffers):
-                    buffer_size = vertex_buffer.vertex_count * 16
-                    for vertex in self._parse_verticies(
-                        self._raw_data[vert_start : vert_start + buffer_size],
-                        vertex_buffer,
-                    ):
+            for dl in self.display_lists:
+                # if len(dl.verticies) != len(dl.triangles):
+                #     continue
+                for verticies, triangles in zip(dl.verticies, dl.triangles):
+                    for vertex in verticies:
                         obj_line = f"v {vertex.x} {vertex.y} {vertex.z}\n"
                         obj_file.write(obj_line)
-                    vert_start += buffer_size
-                    for tri in display_list.triangles[v_buff_num]:
-                        obj_file.write(
-                            f"f {tri.v1 + face_offset} {tri.v2 + face_offset} {tri.v3 + face_offset}\n"
-                        )
-                    face_offset += vertex_buffer.vertex_count
+                        
+                    for tri in triangles:
+                        obj_line = f"f {tri.v1 + tri_offset} {tri.v2 + tri_offset} {tri.v3 + tri_offset}\n"
+                        obj_file.write(obj_line)
+                        
+                    tri_offset += len(verticies)
